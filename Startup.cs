99fb34a4025a490d0771b01b840d23bec7820664
Alexandre Andrade <aralves.andrade@gmail.com>
2021-10-21 21:24:13 -0300

    update

--- Startup.cs
+++ Startup.cs
@@ -2,8 +2,10 @@ using MeuTodo.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace MeuTodo
 {
@@ -12,7 +14,13 @@ namespace MeuTodo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddDbContext<AppDbContext>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -24,12 +32,31 @@ namespace MeuTodo
 
             app.UseRouting();
 
+            UseMigrations(app);
+
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
+        }
+
+        private static void UseMigrations(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+                using (var db = provider.GetRequiredService<AppDbContext>())
+                {
+                    db.Database.Migrate();
+                }
+            }
         }
     }
 }
